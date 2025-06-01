a91f628fd37918c0d42d410e1c74423ff11431f8
asajedi <ali.sajedi@gwdg.de>
2025-06-02 01:46:43 +0200

    Remove commend lines

--- lib/src/main/java/eu/faircore4eosc/pidmr/PIDMRHDLProxy.java
+++ lib/src/main/java/eu/faircore4eosc/pidmr/PIDMRHDLProxy.java
@@ -368,12 +368,7 @@ public class PIDMRHDLProxy extends HDLProxy {
             throw new FileNotFoundException("Provider file not found or not a regular file: " + providersFilePath);
         }
         try (FileReader reader = new FileReader(providersFilePath)) {
-
             return JsonParser.parseReader(reader).getAsJsonObject();
-
-
-//            JsonParser jsonParser = new JsonParser();
-//            return jsonParser.parse(reader).getAsJsonObject();
         }
     }
 
