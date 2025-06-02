afa4dcb8c1947dd8db4fb827ccdb36b191d2e7cc
asajedi <ali.sajedi@gwdg.de>
2025-06-02 15:32:17 +0200

    Introduce provider service
    This introduces the provider service which determins a specific
    provider based on a given PID.

---
+++ lib/src/main/java/eu/faircore4eosc/pidmr/services/ProviderService.java
@@ -0,0 +1,29 @@
+package eu.faircore4eosc.pidmr.services;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class ProviderService {
+    private final Map<String, List<Pattern>> pidPatterns = new HashMap<>();
+
+    public void loadProviders(JsonArray providers) {
+        for (JsonElement provider : providers) {
+            String type = provider.getAsJsonObject().get("type").getAsString();
+            JsonArray regexes = provider.getAsJsonObject().getAsJsonArray("regexes");
+            List<Pattern> patterns = new ArrayList<>();
+            for (JsonElement regex : regexes) {
+                patterns.add(Pattern.compile(regex.getAsString()));
+            }
+            pidPatterns.put(type, patterns);
+        }
+    }
+
+    public String detectPidType(String pid) {
+        return pidPatterns.entrySet().stream()
+                .filter(entry -> entry.getValue().stream().anyMatch(p -> p.matcher(pid).matches()))
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(null);
+    }
+}
