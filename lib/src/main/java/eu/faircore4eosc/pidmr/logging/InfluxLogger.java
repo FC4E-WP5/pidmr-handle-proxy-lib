97ae7714f9ef832077f70debd35fc017209257de
asajedi <ali.sajedi@gwdg.de>
2025-06-13 04:21:03 +0200

    Remove InfluxDB config casting bug

--- lib/src/main/java/eu/faircore4eosc/pidmr/logging/InfluxLogger.java
+++ lib/src/main/java/eu/faircore4eosc/pidmr/logging/InfluxLogger.java
@@ -8,6 +8,7 @@ import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.TimeUnit;
@@ -17,10 +18,10 @@ import eu.faircore4eosc.pidmr.utilities.TimeUtils;
 
 public class InfluxLogger {
 
-    public static void log(ConfigLoader.Config config, String pidType, String pid, String display,
+    public static void log(ConfigLoader.Config pidmrConfig, String pidType, String pid, String display,
                            String redirectUrl, String responseTime, Integer status) {
 
-        String influxdbConfigFile = config.getInfluxdbConfigFile();
+        String influxdbConfigFile = pidmrConfig.getInfluxdbConfigFile();
         StreamTable configTable = new StreamTable();
         File serverDir = new File(HSG.DEFAULT_CONFIG_SUBDIR_NAME);
 
@@ -28,10 +29,20 @@ public class InfluxLogger {
             File configFile = new File(serverDir, influxdbConfigFile);
             if (configFile.exists()) {
                 configTable.readFromFile(configFile);
-
                 if (configTable.containsKey("influxdb_config")) {
-                    Map<String, Object> influxdbConfig = (Map<String, Object>) configTable.get("influxdb_config");
-
+                    Map<String, Object> influxdbConfig = null;
+                    Object config = configTable.get("influxdb_config");
+                    if (config instanceof Map<?, ?> rawMap) {
+                        influxdbConfig = new HashMap<>();
+                        for (Map.Entry<?, ?> entry : rawMap.entrySet()) {
+                            if (entry.getKey() instanceof String) {
+                                influxdbConfig.put((String) entry.getKey(), entry.getValue());
+                            }
+                        }
+                    } else {
+                        System.err.println("configuration error: influxdb_config is invalid");
+                        return;
+                    }
                     String bindAddress = (String) influxdbConfig.get("bind_address");
                     String bindPort = (String) influxdbConfig.get("bind_port");
                     String username = (String) influxdbConfig.get("username");
